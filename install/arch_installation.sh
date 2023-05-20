e2eca5450d6d178f33cfca542bc4067685bb8768
cococry <98212066+cococry@users.noreply.github.com>
2023-05-20 03:37:42 +0200

    Update arch_installation.sh

--- install/arch_installation.sh
+++ install/arch_installation.sh
@@ -3,7 +3,7 @@
 sudo pacman -S xorg xorg-xinit gcc make git freetype2
 git clone https://github.com/cococry/Ragnar.git 
 cd ~/Ragnar
-sudo cp ragnar.desktop /usr/sharexsessions/ragnar.desktop
+sudo cp ragnar.desktop /usr/share/xsessions/ragnar.desktop
 sudo make ragnar install
 echo "! INSTALLED"
 
