7584b12978794e4223bce4bffac23129f4885976
cococry <cococry.btw@gmail.com>
2024-05-27 16:04:20 +0200

    Start alacritty on startup

--- ragnar.c
+++ ragnar.c
@@ -683,7 +683,7 @@ void popupnew(struct wl_listener *listener, void *data) {
 }
 
 void ragnar_init(wm_state* state) {
-	state->display= wl_display_create();
+	state->display = wl_display_create();
 	state->backend = wlr_backend_autocreate(wl_display_get_event_loop(state->display), NULL);
 	if (state->backend == NULL) {
 		wlr_log(WLR_ERROR, "Failed to create Failed wlr_backend");
@@ -775,32 +775,13 @@ void ragnar_terminate(wm_state* state) {
 
 int main(int argc, char *argv[]) {
 	wlr_log_init(WLR_DEBUG, NULL);
-	char *startup_cmd = NULL;
-
-	int c;
-	while ((c = getopt(argc, argv, "s:h")) != -1) {
-		switch (c) {
-		case 's':
-			startup_cmd = optarg;
-			break;
-		default:
-			printf("Usage: %s [-s startup command]\n", argv[0]);
-			return 0;
-		}
-	}
-	if (optind < argc) {
-		printf("Usage: %s [-s startup command]\n", argv[0]);
-		return 0;
-	}
 
 	wm_state state = {0};
   ragnar_init(&state);
 
-	if (startup_cmd) {
-		if (fork() == 0) {
-			execl("/bin/sh", "/bin/sh", "-c", startup_cmd, (void *)NULL);
-		}
-	}
+  if (fork() == 0) {
+    execl("/bin/sh", "/bin/sh", "-c", "alacritty", (void *)NULL);
+  }
 
   ragnar_run(&state);
   ragnar_terminate(&state);
