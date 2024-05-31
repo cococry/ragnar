a3a7b04ad406f1d7e0f030968c39c8b45167c3c7
cococry <cococry.btw@gmail.com>
2024-05-31 09:27:14 +0200

    Fixed adding client to linked list

--- ragnar.c
+++ ragnar.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +9,6 @@
 #include <stdbool.h>
 
 #define _XCB_EV_LAST 35
-#define CLIENT_CAP_INIT 32
 
 #define arraylen(arr) (sizeof(arr) / sizeof(arr[0]))
 
@@ -20,35 +20,40 @@ typedef struct {
   v2 pos, size;
 } area;
 
-static void setup();
-static void loop();
-static void terminate();
-
-static bool pointinarea(v2 p, area a);
-static v2 cursorpos(bool* success);
-static area winarea(xcb_window_t win, bool* success);
-
-static void evmaprequest(xcb_generic_event_t* ev);
-static void evunmapnotify(xcb_generic_event_t* ev);
-static void eventernotify(xcb_generic_event_t* ev);
-
 typedef void (*event_handler_t)(xcb_generic_event_t* ev);
 
-typedef struct {
-  v2 pos, size;
-} client;
+typedef struct client client;
 
-typedef struct {
-  client** data;
-  uint32_t count, cap;
-} client_list;
+struct client {
+  area area;
+  xcb_window_t win;
+
+  client* next;
+};
 
 typedef struct {
   xcb_connection_t* con;
   xcb_window_t root;
 
-  client_list clients;
+  client* clients;
 } State;
+
+
+static void     setup();
+static void     loop();
+static void     terminate();
+
+static bool     pointinarea(v2 p, area a);
+static v2       cursorpos(bool* success);
+static area     winarea(xcb_window_t win, bool* success);
+
+static void     evmaprequest(xcb_generic_event_t* ev);
+static void     evunmapnotify(xcb_generic_event_t* ev);
+static void     eventernotify(xcb_generic_event_t* ev);
+
+static void     addclient(xcb_window_t win);
+static void     removeclient(xcb_window_t win);
+static client*  clientfromwin(xcb_window_t win);
 
 static event_handler_t evhandlers[_XCB_EV_LAST] = {
   [XCB_MAP_REQUEST] = evmaprequest,
@@ -60,6 +65,7 @@ static State s;
 
 void
 setup() {
+  s.clients = NULL;
   s.con = xcb_connect(NULL, NULL);
   if (xcb_connection_has_error(s.con)) {
     fprintf(stderr, "Cannot open display\n");
@@ -150,21 +156,69 @@ evmaprequest(xcb_generic_event_t* ev) {
   }
 flush:
   xcb_flush(s.con);
+
+  addclient(map_ev->window);
 }
 
 void 
 evunmapnotify(xcb_generic_event_t* ev) {
+  // Unmap the window
   xcb_unmap_notify_event_t* unmap_ev = (xcb_unmap_notify_event_t*)ev;
   xcb_unmap_window(s.con, unmap_ev->window);
+
+  // Remove the client from the list
+  removeclient(unmap_ev->window);
 }
 
 void 
 eventernotify(xcb_generic_event_t* ev) {
   xcb_enter_notify_event_t *enter_ev = (xcb_enter_notify_event_t*)ev;
 
+  client* cl = clientfromwin(enter_ev->event);
   // Set input focus on window enter
   xcb_set_input_focus(s.con, XCB_INPUT_FOCUS_POINTER_ROOT, enter_ev->event, XCB_CURRENT_TIME);
   xcb_flush(s.con);
+}
+
+void 
+addclient(xcb_window_t win) {
+  // Allocate client structure
+  client* cl = (client*)malloc(sizeof(*cl));
+  cl->win = win;
+  cl->next = s.clients;
+  
+  // Get the window area
+  bool success;
+  area area = winarea(win, &success);
+  assert(success && "Failed to get window area.");
+  cl->area = area;
+
+  s.clients = cl;
+}
+
+void 
+removeclient(xcb_window_t win) {
+  client** prev = &s.clients;
+  client* cl = s.clients;
+  while(cl) {
+    if(cl->win == win) {
+      *prev = cl->next;
+      free(cl);
+      return;
+    }
+    prev = &cl->next;
+    cl = cl->next;
+  }
+}
+
+client*
+clientfromwin(xcb_window_t win) {
+  client* cl;
+  for(cl = s.clients; cl != NULL; cl = cl->next) {
+    // If the window is found in the clients, return the client
+    if(cl->win == win) return cl;
+  }
+  return NULL;
 }
 
 int 
