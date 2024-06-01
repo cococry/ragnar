05af2bd98d551055542fc57aa0c818aecb27b01d
Cococry <cococry.btw@gmail.com>
2024-06-01 23:00:55 +0200

    Added config.h

---
+++ config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define ALT   XCB_MOD_MASK_1
+#define SUPER XCB_MOD_MASK_4
+
+#define _XCB_MOD_COUNT 6
+
+typedef struct {
+  uint16_t modmask;
+  xcb_keysym_t key;
+} keybind;
+
+static int32_t winborderwidth = 1;
+static int32_t winbordercolor = 0xff0000;
+
+static keybind exitkeybind = {SUPER | ALT, XK_Escape};
+static keybind terminalkeybind = {SUPER, XK_Return};
+
+static const char* terminalcmd = "alacritty &";
