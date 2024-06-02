60a89b4260f1d85ca412cefffb738c5d3206875a
Cococry <cococry.btw@gmail.com>
2024-06-02 10:56:37 +0200

    Outsourced structs & defines into structs.h

--- config.h
+++ config.h
@@ -1,21 +1,10 @@
 #pragma once
-
-typedef enum {
-  Shift     = XCB_MOD_MASK_SHIFT,
-  Control   = XCB_MOD_MASK_CONTROL,
-  Alt       = XCB_MOD_MASK_1,
-  Super     = XCB_MOD_MASK_4
-} kb_modifier;
-
-typedef struct {
-  uint16_t modmask;
-  xcb_keysym_t key;
-} keybind;
+#include "structs.h"
 
 static int32_t winborderwidth = 1;
 static int32_t winbordercolor = 0xff0000;
 
-static keybind exitkeybind = {Super | Alt, XK_Escape};
-static keybind terminalkeybind = {Super, XK_Return};
+static keybind exitkeybind = {Super | Alt, KeyEscape};
+static keybind terminalkeybind = {Super, KeyReturn};
 
 static const char* terminalcmd = "alacritty &";
