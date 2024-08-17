918ca80d419b246d9f9dc3031f7e33decc430c8e
Sigmanificient <edhyjox@gmail.com>
2024-08-17 18:31:04 +0200

    Provide up to date depencencies in flake devShell

--- flake.nix
+++ flake.nix
@@ -2,26 +2,26 @@
   description = "Ragnar Window Manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
 
       libs = with pkgs; [
-        fontconfig
+        libGL
+        mesa
+        libconfig
       ] ++ (with pkgs.xorg; [
         libX11
-        libXft
-        libXcursor
-        libXcomposite
+        libxcb
+        xcbutil
+        xcbutilwm
+        xorgproto
+        xcb-util-cursor
+        xcbutilkeysyms
       ]);
 
     in {
@@ -29,7 +29,6 @@
         packages = with pkgs; [
           gnumake
           gcc
-          glibc
         ] ++ libs;
       };
     });
